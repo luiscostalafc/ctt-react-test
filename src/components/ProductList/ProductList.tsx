74f67833682880c424ea72d7717effd7eeda9045
luiscostalafc <luiscostalafc@gmail.com>
2025-04-08 20:25:21 -0300

    feat: Implements the ProductList component that calls the fetchProducts action and renders the list

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,3 +1,34 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppState } from "../../store";
+import { fetchProducts } from "../../store/products/actions";
+
 export const ProductList = () => {
-  return <h2>Product List</h2>;
+  const dispatch = useDispatch();
+  const { items, loading, error } = useSelector(
+    (state: AppState) => state.products
+  );
+
+  useEffect(() => {
+    dispatch(fetchProducts() as any);
+  }, [dispatch]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+  return (
+    <div>
+      <h2>Products</h2>
+      <ul>
+        {items.map((product) => (
+          <li key={product.id}>
+            {product.description} - ${product.price} (Stock: {product.stock})
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
